7b85a09567ef4768b0899994ff4eb4a356bf0175
Remigijus <remis.in.motion@gmail.com>
2021-06-21 22:49:38 +0300

    makes dataTask and URLRequest in separate more generic methods

--- OhMyGitHub/Managers/NetworkManager.swift
+++ OhMyGitHub/Managers/NetworkManager.swift
@@ -17,19 +17,9 @@ final class NetworkManager {
     
     // MARK: - Helpers
     
-    func makeRequestAndDataTask<T: Codable>(with url: URL,
-                                  method: Method,
+    private func makeDataTask<T: Codable>(with request: URLRequest,
                                   completion: @escaping (Result<T, AppError>) -> ()
     ) -> URLSessionDataTask {
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
-        request.setValue("secret-keyValue", forHTTPHeaderField: "secret-key")
-        
-        if method == .post {
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue("application/json", forHTTPHeaderField: "Accept")
-        }
         
         let dataTask = session.dataTask(with: request) { data, response, error in
             
@@ -49,34 +39,34 @@ final class NetworkManager {
         return dataTask
     }
     
+    private func makePostRequest(with url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = Method.post.rawValue
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        return request
+    }
+    
+    private func makeGetRequest(with url: URL, token: String) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = Method.get.rawValue
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        request.setValue("token \(token)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
     // MARK: - GET tasks
     
     func getAccessToken(_ url: URL, _ completion: @escaping (Result<AccessTokenResponse, AppError>) -> ()) {
-        let dataTask = makeRequestAndDataTask(with: url, method: .post, completion: completion)
+        let request = makePostRequest(with: url)
+        let dataTask = makeDataTask(with: request, completion: completion)
         dataTask.resume()
     }
     
     func getGitHubUser(token: String, _ completion: @escaping (Result<GitHubUser, AppError>) -> ()) {
-        
-        var request = URLRequest(url: URL(string: "https://api.github.com/user")!)
-        request.httpMethod = Method.get.rawValue
-        request.setValue("token \(token)", forHTTPHeaderField: "Authorization")
-        
-        let dataTask = session.dataTask(with: request) { data, response, error in
-            
-            guard let data = data else {
-                completion(.failure(.firstError))
-                return
-            }
-
-            self.decoder.keyDecodingStrategy = .convertFromSnakeCase
-            guard let decodedData = try? self.decoder.decode(GitHubUser.self, from: data) else {
-                completion(.failure(.decodingFailed))
-                return
-            }
-
-            completion(.success(decodedData))
-        }
+        // TODO: Deal with Endpoints in one place, no hardcoding
+        let url = URL(string: "https://api.github.com/user")!
+        let request = makeGetRequest(with: url, token: token)
+        let dataTask = makeDataTask(with: request, completion: completion)
         dataTask.resume()
     }
 }
